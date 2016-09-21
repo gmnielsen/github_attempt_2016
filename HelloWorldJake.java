75bba3eb1afc754696bd71f6030cb8980191f974
AliceLiu97 <liu.alice@cheverus.org>
2016-09-21 16:00:12 -0400

    alice
    a

--- HelloWorldJake.java
+++ HelloWorldJake.java
@@ -3,6 +3,7 @@ public class HelloWorldJake{
 	public static void main (String[] args){
 		System.out.println("Hello World - sorry, no fun");
 		System.out.println("Hello World - oops, no fun");
+		System.out.println("Hello World - alice");
 	}
 	
 	
